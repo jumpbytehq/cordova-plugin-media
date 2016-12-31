d03822c0bccb8e910efee06a0b8e2af68e9c5226
Ronit <ronit@jumpbyte.com>
2016-12-31 18:31:55 +0530

    android minor bug fixed in setRate

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -322,6 +322,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             try {
                 int currentPosision = player.getCurrentPosition();
+                mediaPlayer.pause();
                 player.reset();
                 player.setDataSource(file);
                 player.prepare();
